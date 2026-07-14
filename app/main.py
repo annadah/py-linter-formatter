28e3926228a8e15d4b1238c21e06fc5b66f2f3d5
Anna Dakhno <anna099417@gmail.com>
2026-07-15 00:34:13 +0300

    solving y-linter-formatter2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def format_linter_error(error: dict) -> dict:
     return {
-    "line" : error["line_number"],
+        "line" : error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
@@ -16,8 +16,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 
 
-
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
